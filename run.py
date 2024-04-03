1490827e8083e9179e657cc0a4ddd32e6b2284e2
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-04-03 09:19:12 +0000

    added Initialize game boards function

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+
+def initialize_game_boards():
+    '''
+        Initialize game boards before the game
+    '''
+
+    
+    player_hidden_pattern = [[' '] * 8 for _ in range(8)]
+    player_guess_pattern = [[' '] * 8 for _ in range(8)]
+    computer_hidden_pattern = [[' '] * 8 for _ in range(8)]
+    computer_guess_pattern = [[' '] * 8 for _ in range(8)]
+
+    return (player_hidden_pattern,
+            player_guess_pattern,
+            computer_hidden_pattern,
+            computer_guess_pattern)
+
