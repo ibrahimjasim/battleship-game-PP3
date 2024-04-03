ac8d618bbbb7cd769f515a66996246ef953d0e49
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-04-03 09:21:18 +0000

    added Function to print two game boards side by side

--- run.py
+++ run.py
@@ -16,4 +16,31 @@ def initialize_game_boards():
             player_guess_pattern,
             computer_hidden_pattern,
             computer_guess_pattern)
+            
 
+def print_boards(board1, board2,
+                 board1_title="Your Guesses", board2_title="Computer's Guesses"):
+    '''
+        Function to print two game boards side by side
+
+        input : 
+            board1 : list of characters related to the user
+            board2 : list of characters related to the computer
+            board1_title : title of the user board
+            board2_title : title of the computer board
+
+        output : 
+            prints the title of two boards and then the boards themselves
+
+    '''
+
+    
+    print(f'{board1_title:20s}    {board2_title}')
+    print('   A B C D E F G H      A B C D E F G H')
+    print('   ****************     ****************')
+
+    
+    for row_num in range(8):
+        row1 = " ".join(board1[row_num])
+        row2 = " ".join(board2[row_num])
+        print(f"{row_num + 1}|{row1}|    {row_num + 1}|{row2}|")
